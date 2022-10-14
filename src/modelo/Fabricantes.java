147f38d497e73e9a0ff268260f00dbc3fd704c2c
Enrique Herrera <bycrashdash@gmail.com>
2022-10-14 17:15:04 -0500

    se agrego el modelo fabricantes

---
+++ src/modelo/Fabricantes.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author Herrera
+ */
+public class Fabricantes {
+    private int idFabricante;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+    public Fabricantes() {
+    }
+
+    public Fabricantes(int idFabricante, String nombre, String direccion, String telefono) {
+        this.idFabricante = idFabricante;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public int getIdFabricante() {
+        return idFabricante;
+    }
+
+    public void setIdFabricante(int idFabricante) {
+        this.idFabricante = idFabricante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    } 
+
+    @Override
+    public String toString() {
+        return "Fabricantes{" + "idFabricante=" + idFabricante + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + '}';
+    }
+    
+    
+}
